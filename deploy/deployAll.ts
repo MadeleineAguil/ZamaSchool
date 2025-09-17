e8dd75768278c48ad066d7634eb388ade2e1f1c1
MadeleineAguil <GaryVega1986120@hotmail.com>
2025-09-17 22:27:17 +0800

    deploy contract

---
+++ deploy/deployAll.ts
@@ -0,0 +1,31 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy } = hre.deployments;
+
+  console.log("Deploying all ZamaSchool contracts...");
+  console.log("Deployer address:", deployer);
+
+  // 部署 NumberStorage 合约
+  const deployedNumberStorage = await deploy("NumberStorage", {
+    from: deployer,
+    log: true,
+  });
+
+  // 部署 AddressStorage 合约
+  const deployedAddressStorage = await deploy("AddressStorage", {
+    from: deployer,
+    log: true,
+  });
+
+  console.log("\n=== Deployment Summary ===");
+  console.log(`NumberStorage contract: ${deployedNumberStorage.address}`);
+  console.log(`AddressStorage contract: ${deployedAddressStorage.address}`);
+  console.log("===========================\n");
+};
+
+export default func;
+func.id = "deploy_all_zamaschool";
+func.tags = ["ZamaSchool", "All"];
