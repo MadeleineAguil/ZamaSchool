cae6f3be2e32824166c7a683c984d809e9108c16
MadeleineAguil <GaryVega1986120@hotmail.com>
2025-09-20 18:23:47 +0800

    add compare func

--- contracts/NumberStorage.sol
+++ contracts/NumberStorage.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {FHE, euint32, externalEuint32} from "@fhevm/solidity/lib/FHE.sol";
+import {FHE, euint32, externalEuint32, ebool} from "@fhevm/solidity/lib/FHE.sol";
 import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
 
 contract NumberStorage is SepoliaConfig {
     mapping(address => euint32) private userNumbers;
     mapping(address => euint32) private calculationResults;
+    mapping(address => ebool) private comparisonResults;
 
     event NumberStored(address indexed user);
     event CalculationPerformed(address indexed user, string operation);
+    event ComparisonPerformed(address indexed user, string comparisonType);
 
     function storeNumber(externalEuint32 inputNumber, bytes calldata inputProof) external {
         euint32 encryptedNumber = FHE.fromExternal(inputNumber, inputProof);
@@ -28,6 +30,10 @@ contract NumberStorage is SepoliaConfig {
 
     function getCalculationResult(address user) external view returns (euint32) {
         return calculationResults[user];
+    }
+
+    function getComparisonResult(address user) external view returns (ebool) {
+        return comparisonResults[user];
     }
 
     // 加法运算
@@ -103,5 +109,107 @@ contract NumberStorage is SepoliaConfig {
         FHE.allow(calculationResults[msg.sender], msg.sender);
 
         emit CalculationPerformed(msg.sender, "add_two_users");
+    }
+
+    // === 数字比较功能 ===
+
+    // 比较存储的数字是否等于输入数字
+    function compareStoredNumberEqual(externalEuint32 inputNumber, bytes calldata inputProof) external {
+        require(FHE.isInitialized(userNumbers[msg.sender]), "No stored number found");
+
+        euint32 numberToCompare = FHE.fromExternal(inputNumber, inputProof);
+        ebool result = FHE.eq(userNumbers[msg.sender], numberToCompare);
+
+        comparisonResults[msg.sender] = result;
+
+        FHE.allowThis(comparisonResults[msg.sender]);
+        FHE.allow(comparisonResults[msg.sender], msg.sender);
+
+        emit ComparisonPerformed(msg.sender, "equal");
+    }
+
+    // 比较存储的数字是否大于输入数字
+    function compareStoredNumberGreater(externalEuint32 inputNumber, bytes calldata inputProof) external {
+        require(FHE.isInitialized(userNumbers[msg.sender]), "No stored number found");
+
+        euint32 numberToCompare = FHE.fromExternal(inputNumber, inputProof);
+        ebool result = FHE.gt(userNumbers[msg.sender], numberToCompare);
+
+        comparisonResults[msg.sender] = result;
+
+        FHE.allowThis(comparisonResults[msg.sender]);
+        FHE.allow(comparisonResults[msg.sender], msg.sender);
+
+        emit ComparisonPerformed(msg.sender, "greater");
+    }
+
+    // 比较存储的数字是否小于输入数字
+    function compareStoredNumberLess(externalEuint32 inputNumber, bytes calldata inputProof) external {
+        require(FHE.isInitialized(userNumbers[msg.sender]), "No stored number found");
+
+        euint32 numberToCompare = FHE.fromExternal(inputNumber, inputProof);
+        ebool result = FHE.lt(userNumbers[msg.sender], numberToCompare);
+
+        comparisonResults[msg.sender] = result;
+
+        FHE.allowThis(comparisonResults[msg.sender]);
+        FHE.allow(comparisonResults[msg.sender], msg.sender);
+
+        emit ComparisonPerformed(msg.sender, "less");
+    }
+
+    // 比较存储的数字是否大于等于输入数字
+    function compareStoredNumberGreaterOrEqual(externalEuint32 inputNumber, bytes calldata inputProof) external {
+        require(FHE.isInitialized(userNumbers[msg.sender]), "No stored number found");
+
+        euint32 numberToCompare = FHE.fromExternal(inputNumber, inputProof);
+        ebool result = FHE.ge(userNumbers[msg.sender], numberToCompare);
+
+        comparisonResults[msg.sender] = result;
+
+        FHE.allowThis(comparisonResults[msg.sender]);
+        FHE.allow(comparisonResults[msg.sender], msg.sender);
+
+        emit ComparisonPerformed(msg.sender, "greater_or_equal");
+    }
+
+    // 比较存储的数字是否小于等于输入数字
+    function compareStoredNumberLessOrEqual(externalEuint32 inputNumber, bytes calldata inputProof) external {
+        require(FHE.isInitialized(userNumbers[msg.sender]), "No stored number found");
+
+        euint32 numberToCompare = FHE.fromExternal(inputNumber, inputProof);
+        ebool result = FHE.le(userNumbers[msg.sender], numberToCompare);
+
+        comparisonResults[msg.sender] = result;
+
+        FHE.allowThis(comparisonResults[msg.sender]);
+        FHE.allow(comparisonResults[msg.sender], msg.sender);
+
+        emit ComparisonPerformed(msg.sender, "less_or_equal");
+    }
+
+    // 比较两个用户存储的数字大小
+    function compareTwoUsersNumbers(address userA, address userB, string calldata comparisonType) external {
+        require(FHE.isInitialized(userNumbers[userA]), "UserA has no stored number");
+        require(FHE.isInitialized(userNumbers[userB]), "UserB has no stored number");
+
+        ebool result;
+
+        if (keccak256(abi.encodePacked(comparisonType)) == keccak256(abi.encodePacked("equal"))) {
+            result = FHE.eq(userNumbers[userA], userNumbers[userB]);
+        } else if (keccak256(abi.encodePacked(comparisonType)) == keccak256(abi.encodePacked("greater"))) {
+            result = FHE.gt(userNumbers[userA], userNumbers[userB]);
+        } else if (keccak256(abi.encodePacked(comparisonType)) == keccak256(abi.encodePacked("less"))) {
+            result = FHE.lt(userNumbers[userA], userNumbers[userB]);
+        } else {
+            revert("Invalid comparison type. Use: equal, greater, less");
+        }
+
+        comparisonResults[msg.sender] = result;
+
+        FHE.allowThis(comparisonResults[msg.sender]);
+        FHE.allow(comparisonResults[msg.sender], msg.sender);
+
+        emit ComparisonPerformed(msg.sender, string(abi.encodePacked("compare_users_", comparisonType)));
     }
 }
